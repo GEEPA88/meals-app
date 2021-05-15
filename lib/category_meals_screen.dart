8065ec282fa0bab6943c62ba7010c96ddf7a91cd
GEEPA88 <geepaabhijith@gmail.com>
2021-05-15 16:01:17 +0530

    pushing to other screens

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  @override
+  final String categoryId;
+  final String categoryTitle;
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'The Recipes',
+          categoryTitle,
         ),
       ),
       body: Center(
