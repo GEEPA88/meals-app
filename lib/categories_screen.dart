edacdfa7e8c349186a29da953ba8947cb86b5659
GEEPA88 <geepaabhijith@gmail.com>
2021-05-15 15:50:54 +0530

    navigator push

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,8 +10,14 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Delimeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.title, catData.color))
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
